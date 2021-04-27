3dd5bf6cf66c8a9ca9d00c557f384cc511e02c5f
Hamza <ha42226@gmail.com>
2021-04-27 05:26:09 -0700

    update

--- tesseract.py
+++
@@ -1,7 +0,0 @@
-
-import cv2 
-import pytesseract
-
-img =cv2.imread('C:/Users/Hamza/project/papers/media/image.jpeg');
-custom_config = r'--oem 3 --psm 6'
-print(pytesseract.image_to_string(img, config=custom_config))
