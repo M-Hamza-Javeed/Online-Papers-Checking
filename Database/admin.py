2c577ff22ada7e8d9789626fa3fc073fb6c394e4
Hamza <ha42226@gmail.com>
2021-05-08 00:01:30 -0700

    update

--- Database/admin.py
+++ Database/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.apps import apps
-# from .models import Admin,Student,Teacher,Courses,TeacherSubject,\
-#     Exam,ResultStudent,Mcqs,McqsChoice,Questions,ExamMcqs,ExamMcqsChoice,ExamQuestions
+from .models import Admin,Student,Teacher,Courses,TeacherSubject,\
+    Exam,Mcqs,McqsChoice,Questions,ExamMcqs,ExamMcqsChoice,ExamQuestions,ResultStubject
 
-# admin.site.register([Admin,Student,Teacher,Courses,TeacherSubject,
-# Exam,ResultStudent,Mcqs,McqsChoice,Questions,ExamMcqs,ExamMcqsChoice,ExamQuestions])
+
+admin.site.register([Admin,Student,Teacher,Courses,TeacherSubject,
+Exam,ResultStubject,Mcqs,McqsChoice,Questions,ExamMcqs,ExamMcqsChoice,ExamQuestions])
